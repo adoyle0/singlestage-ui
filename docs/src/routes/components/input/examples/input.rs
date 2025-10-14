ce4d8d9c24e018d28902a071839c01be97321164
Adam Doyle <adam@doordesk.net>
2025-10-14 03:12:52 -0400

    feat: easy labeling for `Input`s

--- docs/src/routes/components/input/examples/input.rs
+++ docs/src/routes/components/input/examples/input.rs
@@ -5,22 +5,28 @@ use singlestage::{input::*, label::*};
 pub fn InputExample() -> impl IntoView {
     view! {
         <div class="space-y-4">
-            <Label class="grid gap-2">"Color:"<Input input_type="color" default="#F00" /></Label>
-            <Label class="grid gap-2">"Date:"<Input input_type="date" /></Label>
-            <Label class="grid gap-2">"Datetime:"<Input input_type="datetime-local" /></Label>
-            <Label class="grid gap-2">"Email:"<Input input_type="email" /></Label>
-            <Label class="grid gap-2">"File:"<Input input_type="file" /></Label>
+            <Input input_type="color" default="#F00">
+                "Color:"
+            </Input>
+            <Input input_type="date">"Date:"</Input>
+            <Input input_type="datetime-local">"Datetime:"</Input>
+            <Input input_type="email">"Email:"</Input>
+            <Input input_type="file">"File:"</Input>
             <Input input_type="hidden" name="John Cena" />
-            <Label class="grid gap-2">"Month:"<Input input_type="month" /></Label>
-            <Label class="grid gap-2">"Number:"<Input input_type="number" /></Label>
-            <Label class="grid gap-2">"Password:"<Input input_type="password" /></Label>
-            <Label class="grid gap-2">"Search:"<Input input_type="search" /></Label>
-            <Label class="grid gap-2">"Phone Number:"<Input input_type="tel" /></Label>
-            <Label class="grid gap-2">"Disabled:"<Input input_type="text" disabled=true /></Label>
-            <Label class="grid gap-2">"Invalid:"<Input input_type="text" invalid=true /></Label>
-            <Label class="grid gap-2">"Time:"<Input input_type="time" /></Label>
-            <Label class="grid gap-2">"URL:"<Input input_type="url" /></Label>
-            <Label class="grid gap-2">"Week:"<Input input_type="week" /></Label>
+            <Input input_type="month">"Month:"</Input>
+            <Input input_type="number">"Number:"</Input>
+            <Input input_type="password">"Password:"</Input>
+            <Input input_type="search">"Search:"</Input>
+            <Input input_type="tel">"Phone Number:"</Input>
+            <Input input_type="text" disabled=true>
+                "Disabled:"
+            </Input>
+            <Input input_type="text" invalid=true>
+                "Invalid:"
+            </Input>
+            <Input input_type="time">"Time:"</Input>
+            <Input input_type="url">"URL:"</Input>
+            <Input input_type="week">"Week:"</Input>
         </div>
     }
 }
