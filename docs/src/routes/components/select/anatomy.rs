b86ae5f842bb87fba64f980d3acecbebd8f514b1
Adam Doyle <24621027+adoyle0@users.noreply.github.com>
2025-10-15 03:16:48 -0400

    refactor: add SelectContent and SelectItem sub-components and update docs (#3)
    * refactor: add `SelectContent` and `SelectItem` sub-components and update docs

--- docs/src/routes/components/select/anatomy.rs
+++ docs/src/routes/components/select/anatomy.rs
@@ -4,14 +4,10 @@ use singlestage::select::*;
 #[component]
 pub fn SelectAnatomy() -> impl IntoView {
     view! {
-        <Select class="w-[180px]">
-            <optgroup label="Fruits">
-                <option>"Apple"</option>
-                <option>"Banana"</option>
-                <option>"Blueberry"</option>
-                <option>"Grapes"</option>
-                <option>"Pineapple"</option>
-            </optgroup>
+        <Select>
+            <SelectContent>
+                <SelectItem />
+            </SelectContent>
         </Select>
     }
 }
