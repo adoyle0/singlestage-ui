350e5659ecb59d66716a941df9f9cf24d5863a09
D'mitri Joseph <dmitri.joseph@hinge.co>
2025-10-18 12:00:05 -0400

    fix toggle_with_text example

--- docs/src/routes/components/toggle/examples/toggle_with_text.rs
+++ docs/src/routes/components/toggle/examples/toggle_with_text.rs
@@ -5,7 +5,7 @@ use singlestage::*;
 #[component]
 pub fn ToggleWithTextExample() -> impl IntoView {
     view! {
-        <Toggle variant="outline" aria_label="Toggle italic">
+        <Toggle aria_label="Toggle italic">
             {icon!(LuItalic)}
             "Italic"
         </Toggle>
