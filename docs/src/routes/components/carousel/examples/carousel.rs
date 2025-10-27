c8ef1ee92875e0f9298a61a9e0a8e0de19cfc3f5
Adam Doyle <adam@doordesk.net>
2025-10-26 23:30:09 -0400

    clean up example

--- docs/src/routes/components/carousel/examples/carousel.rs
+++ docs/src/routes/components/carousel/examples/carousel.rs
@@ -8,24 +8,15 @@ pub fn CarouselExample() -> impl IntoView {
     view! {
         <Carousel class="w-full max-w-xs">
             <CarouselContent>
-                <For
-                    each=move || carousel_items.get()
-                    key=|item| item.clone()
-                    children=move |item| {
-                        view! {
-                            <CarouselItem>
-                                <Card>
-                                    <CardContent class="flex aspect-square items-center
-                                    justify-center p-6">
-                                        <span class="text-4xl font-semibold">
-                                            {item.to_string()}
-                                        </span>
-                                    </CardContent>
-                                </Card>
-                            </CarouselItem>
-                        }
-                    }
-                />
+                <For each=move || carousel_items.get() key=|item| item.clone() let(item)>
+                    <CarouselItem>
+                        <Card>
+                            <CardContent class="flex aspect-square items-center justify-center">
+                                <span class="text-4xl font-semibold">{item.to_string()}</span>
+                            </CardContent>
+                        </Card>
+                    </CarouselItem>
+                </For>
             </CarouselContent>
             <CarouselPrevious />
             <CarouselNext />
