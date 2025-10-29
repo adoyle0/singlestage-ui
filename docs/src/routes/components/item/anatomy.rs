1408e0e6aeb6d3016db5a744ac93ee6fc990fd6a
Adam Doyle <adam@doordesk.net>
2025-10-29 04:19:48 -0400

    update item anatomy

--- docs/src/routes/components/item/anatomy.rs
+++ docs/src/routes/components/item/anatomy.rs
@@ -3,5 +3,21 @@ use singlestage::*;
 
 #[component]
 pub fn ItemAnatomy() -> impl IntoView {
-    view! { <Item></Item> }
+    view! {
+        <ItemGroup>
+            <Item>
+                <ItemMedia />
+                <ItemHeader />
+                <ItemContent>
+                    <ItemTitle />
+                    <ItemDescription />
+                </ItemContent>
+                <ItemActions>
+                    <Button />
+                </ItemActions>
+                <ItemFooter />
+            </Item>
+            <ItemSeparator />
+        </ItemGroup>
+    }
 }
