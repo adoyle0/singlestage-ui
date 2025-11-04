99f718cbd61c40d0e6c0650fcb0bcc3b58772c67
Adam Doyle <adam@doordesk.net>
2025-11-04 17:43:32 -0500

    fix landing page layout

--- docs/src/routes/landing/mod.rs
+++ docs/src/routes/landing/mod.rs
@@ -7,31 +7,33 @@ use demos::*;
 #[component]
 pub fn LandingPage() -> impl IntoView {
     view! {
-        <h1 class="text-4xl font-semibold">"A familiar UI library for Leptos"</h1>
-        <p class="text-(--muted-foreground) pt-3">
-            "Made with " <Link href="https://tailwindcss.com">"Tailwind CSS"</Link> ", Based on "
-            <Link href="https://basecoatui.com">"Basecoat"</Link> ", "
-            <Link href="https://ui.shadcn.com">"shadcn/ui"</Link> ", and "
-            <Link href="https://radix-ui.com">"Radix"</Link> "."
-        </p>
+        <header class="w-fit mx-auto lg:ml-4 lg:mr-auto">
+            <h1 class="text-4xl font-semibold">"A familiar UI library for Leptos"</h1>
+            <p class="text-(--muted-foreground) pt-3">
+                "Made with " <Link href="https://tailwindcss.com">"Tailwind CSS"</Link>
+                ", Based on " <Link href="https://basecoatui.com">"Basecoat"</Link> ", "
+                <Link href="https://ui.shadcn.com">"shadcn/ui"</Link> ", and "
+                <Link href="https://radix-ui.com">"Radix"</Link> "."
+            </p>
 
-        <div class="my-6 space-x-2">
-            <a href="install">
-                <Button>"Get Started"</Button>
-            </a>
-            <a href="introduction">
-                <Button variant="outline">"Learn More"</Button>
-            </a>
-        </div>
+            <div class="my-6 space-x-2">
+                <a href="install">
+                    <Button>"Get Started"</Button>
+                </a>
+                <a href="introduction">
+                    <Button variant="outline">"Learn More"</Button>
+                </a>
+            </div>
+        </header>
 
-        <section class="grid grid-cols-1 xl:grid-cols-2 gap-4 mt-8 max-w-fit mr-auto">
-            <div class="flex flex-col gap-4 max-w-lg">
+        <section class="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-8 max-w-fit mx-auto lg:ml-4 lg:mr-auto">
+            <div class="flex flex-col gap-4 max-w-md">
                 <TeamMembers />
                 <CookieSettings />
                 <PaymentMethod />
             </div>
 
-            <div class="flex flex-col gap-4 max-w-lg">
+            <div class="flex flex-col gap-4 max-w-md">
                 <ChatDemo />
                 <CreateAccount />
                 <ReportIssue />
