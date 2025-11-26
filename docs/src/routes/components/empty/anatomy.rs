7340fccb6ac81e7b3fa9c386e4718d3d8b93cac8
Adam Doyle <adam@doordesk.net>
2025-11-25 20:26:29 -0500

    empty anatomy

--- docs/src/routes/components/empty/anatomy.rs
+++ docs/src/routes/components/empty/anatomy.rs
@@ -3,5 +3,14 @@ use singlestage::Empty::*;
 
 #[component]
 pub fn EmptyAnatomy() -> impl IntoView {
-    view! { <Empty /> }
+    view! {
+        <Empty>
+            <EmptyHeader>
+                <EmptyMedia />
+                <EmptyTitle />
+                <EmptyDescription />
+            </EmptyHeader>
+            <EmptyContent />
+        </Empty>
+    }
 }
