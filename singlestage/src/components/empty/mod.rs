a7917482384a76f9ee9120c63508141e4e53e2ed
Adam Doyle <adam@doordesk.net>
2025-11-25 20:24:09 -0500

    finish empty

--- singlestage/src/components/empty/mod.rs
+++ singlestage/src/components/empty/mod.rs
@@ -1,81 +1,13 @@
-use leptos::prelude::*;
+mod content;
+mod description;
+mod empty;
+mod header;
+mod media;
+mod title;
 
-#[component]
-pub fn Empty(
-    children: Children,
-    #[prop(optional, into)] class: MaybeProp<String>,
-) -> impl IntoView {
-    view! {
-        <div class=move || {
-            format!("singlestage-empty {}", class.get().unwrap_or_default())
-        }>{children()}</div>
-    }
-}
-
-#[component]
-pub fn EmptyHeader(
-    children: Children,
-    #[prop(optional, into)] class: MaybeProp<String>,
-) -> impl IntoView {
-    view! {
-        <div class=move || {
-            format!("singlestage-empty-header {}", class.get().unwrap_or_default())
-        }>{children()}</div>
-    }
-}
-
-#[component]
-pub fn EmptyMedia(
-    children: Children,
-    #[prop(optional, into)] class: MaybeProp<String>,
-    #[prop(optional, into)] variant: MaybeProp<String>,
-) -> impl IntoView {
-    view! {
-        <div class=move || {
-            format!(
-                "{} {}",
-                match variant.get().unwrap_or_default().as_str() {
-                    "icon" => "singlestage-empty-media-icon",
-                    _ => "singlestage-empty-media",
-                },
-                class.get().unwrap_or_default(),
-            )
-        }>{children()}</div>
-    }
-}
-
-#[component]
-pub fn EmptyTitle(
-    children: Children,
-    #[prop(optional, into)] class: MaybeProp<String>,
-) -> impl IntoView {
-    view! {
-        <div class=move || {
-            format!("singlestage-empty-title {}", class.get().unwrap_or_default())
-        }>{children()}</div>
-    }
-}
-
-#[component]
-pub fn EmptyDescription(
-    children: Children,
-    #[prop(optional, into)] class: MaybeProp<String>,
-) -> impl IntoView {
-    view! {
-        <div class=move || {
-            format!("singlestage-empty-description {}", class.get().unwrap_or_default())
-        }>{children()}</div>
-    }
-}
-
-#[component]
-pub fn EmptyContent(
-    children: Children,
-    #[prop(optional, into)] class: MaybeProp<String>,
-) -> impl IntoView {
-    view! {
-        <div class=move || {
-            format!("singlestage-empty-content {}", class.get().unwrap_or_default())
-        }>{children()}</div>
-    }
-}
+pub use content::*;
+pub use description::*;
+pub use empty::*;
+pub use header::*;
+pub use media::*;
+pub use title::*;
