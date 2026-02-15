f3778f0e8afe6108c2fefee6fdcb72cbec264847
Adam Doyle <adam@doordesk.net>
2026-02-14 19:13:44 -0500

    update `Progress`

--- docs/src/routes/components/progress/examples/mod.rs
+++ docs/src/routes/components/progress/examples/mod.rs
@@ -1,3 +1,7 @@
 mod progress;
+mod progress_controlled;
+mod progress_label;
 
 pub use progress::*;
+pub use progress_controlled::*;
+pub use progress_label::*;
--- singlestage/src/components/progress/mod.rs
+++ singlestage/src/components/progress/mod.rs
@@ -5,10 +5,10 @@ use leptos::prelude::*;
 pub fn Progress(
     /// The value that represents 100%, or full.
     #[prop(optional, into)]
-    max: MaybeProp<usize>,
+    max: MaybeProp<f64>,
     /// The current progress value to be rendered.
     #[prop(optional, into)]
-    value: MaybeProp<usize>,
+    value: MaybeProp<f64>,
 
     // GLOBAL ATTRIBUTES
     //
@@ -155,23 +155,13 @@ pub fn Progress(
     };
 
     view! {
-        <div
+        <progress
             class=move || format!("singlestage-progress {}", class.get().unwrap_or_default())
+            max=move || max.get().unwrap_or(100.)
+            value=move || value.get()
 
             {..global_attrs_1}
             {..global_attrs_2}
-        >
-            <div
-                class="singlestage-progress-indicator"
-                style=move || {
-                    format!(
-                        "transform: translateX(-{}%)",
-                        100.
-                            - ((value.get().unwrap_or_default() as f64
-                                / max.get().unwrap_or(100) as f64) * 100.),
-                    )
-                }
-            ></div>
-        </div>
+        />
     }
 }
