e48006cc81c9199a0512f7597ce99a203f461798
Adam Doyle <adam@doordesk.net>
2026-04-01 02:17:58 -0400

    add/update skeleton examples

--- docs/src/routes/components/skeleton/examples/mod.rs
+++ docs/src/routes/components/skeleton/examples/mod.rs
@@ -1,5 +1,13 @@
 mod skeleton;
+mod skeleton_avatar;
 mod skeleton_card;
+mod skeleton_form;
+mod skeleton_table;
+mod skeleton_text;
 
 pub use skeleton::*;
+pub use skeleton_avatar::*;
 pub use skeleton_card::*;
+pub use skeleton_form::*;
+pub use skeleton_table::*;
+pub use skeleton_text::*;
---
+++ docs/src/routes/components/skeleton/examples/skeleton_text.rs
@@ -0,0 +1,13 @@
+use leptos::prelude::*;
+use singlestage::*;
+
+#[component]
+pub fn SkeletonTextExample() -> impl IntoView {
+    view! {
+        <div class="flex w-full max-w-xs flex-col gap-2">
+            <Skeleton class="h-4 w-full" />
+            <Skeleton class="h-4 w-full" />
+            <Skeleton class="h-4 w-3/4" />
+        </div>
+    }
+}
