7e8e23d618cd67e0b3dd07bb82f322ce5aec3545
Adam Doyle <adam@doordesk.net>
2026-06-11 16:45:58 -0400

    simplify field choice card

--- docs/src/routes/components/field/examples/field_choice_card.rs
+++ docs/src/routes/components/field/examples/field_choice_card.rs
@@ -4,37 +4,33 @@ use singlestage::*;
 #[component]
 pub fn FieldChoiceCardExample() -> impl IntoView {
     view! {
-        <FieldGroup class="w-full max-w-xs">
-            <FieldSet>
-                <FieldLegend variant="label">"Compute Environment"</FieldLegend>
-                <FieldDescription>
-                    "Select the compute environment for your cluster."
-                </FieldDescription>
-                <RadioGroup value="kubernetes">
-                    <FieldLabel label_for="kubernetes-r2h">
-                        <Field orientation="horizontal">
-                            <FieldContent>
-                                <FieldTitle>"Kubernetes"</FieldTitle>
-                                <FieldDescription>
-                                    "Run GPU workloads on a K8s cluster."
-                                </FieldDescription>
-                            </FieldContent>
-                            <Radio value="kubernetes" id="kubernetes-r2h" />
-                        </Field>
-                    </FieldLabel>
-                    <FieldLabel label_for="vm-z4k">
-                        <Field orientation="horizontal">
-                            <FieldContent>
-                                <FieldTitle>"Virtual Machine"</FieldTitle>
-                                <FieldDescription>
-                                    "Access a cluster to run GPU workloads."
-                                </FieldDescription>
-                            </FieldContent>
-                            <Radio value="vm" id="vm-z4k" />
-                        </Field>
-                    </FieldLabel>
-                </RadioGroup>
-            </FieldSet>
-        </FieldGroup>
+        <FieldSet class="w-full max-w-xs">
+            <FieldLegend variant="label">"Compute Environment"</FieldLegend>
+            <FieldDescription>"Select the compute environment for your cluster."</FieldDescription>
+            <RadioGroup value="kubernetes">
+                <FieldLabel label_for="kubernetes-r2h">
+                    <Field orientation="horizontal">
+                        <FieldContent>
+                            <FieldTitle>"Kubernetes"</FieldTitle>
+                            <FieldDescription>
+                                "Run GPU workloads on a K8s cluster."
+                            </FieldDescription>
+                        </FieldContent>
+                        <Radio value="kubernetes" id="kubernetes-r2h" />
+                    </Field>
+                </FieldLabel>
+                <FieldLabel label_for="vm-z4k">
+                    <Field orientation="horizontal">
+                        <FieldContent>
+                            <FieldTitle>"Virtual Machine"</FieldTitle>
+                            <FieldDescription>
+                                "Access a cluster to run GPU workloads."
+                            </FieldDescription>
+                        </FieldContent>
+                        <Radio value="vm" id="vm-z4k" />
+                    </Field>
+                </FieldLabel>
+            </RadioGroup>
+        </FieldSet>
     }
 }
