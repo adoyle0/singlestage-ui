b14093250cc054f7705ea87ca277f386e83e13a6
Adam Doyle <adam@doordesk.net>
2026-06-12 00:59:36 -0400

    sidebar/css tweaks

--- docs/src/routes/mod.rs
+++ docs/src/routes/mod.rs
@@ -1,7 +1,9 @@
 mod components;
 mod getting_started;
 mod landing;
+mod sidebar_examples;
 
 pub use components::*;
 pub use getting_started::*;
 pub use landing::*;
+pub use sidebar_examples::*;
--- singlestage/src/components/sidebar/rail.rs
+++ singlestage/src/components/sidebar/rail.rs
@@ -1,8 +1,14 @@
-// TODO: Work on this
-
+use crate::SidebarContext;
 use leptos::prelude::*;
 
 #[component]
 pub fn SidebarRail() -> impl IntoView {
-    view! { <button class="singlestage-sidebar-rail" /> }
+    let sidebar = expect_context::<SidebarContext>();
+
+    view! {
+        <button
+            class="singlestage-sidebar-rail"
+            on:click=move |_| sidebar.open.set(!sidebar.open.get_untracked())
+        />
+    }
 }
