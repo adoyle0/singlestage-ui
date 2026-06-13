e0af300ed172ed7c62feb13b544ff5075040a014
Adam Doyle <adam@doordesk.net>
2026-06-12 22:59:58 -0400

    handle web-sys using different types

--- singlestage/src/components/carousel/carousel.rs
+++ singlestage/src/components/carousel/carousel.rs
@@ -139,7 +139,7 @@ pub fn Carousel(
             let width = ul_ref.scroll_width();
             let scroll_step = width / num_items;
 
-            ul_ref.set_scroll_left(scroll_step * (cur - 1));
+            ul_ref.set_scroll_left((scroll_step * (cur - 1)).into());
         }
     });
 
